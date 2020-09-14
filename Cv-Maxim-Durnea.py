e7d7f8daebaea2155e4a83dcb6ec4fedd479e746
Maxim Durnea <70955732+BarSick69@users.noreply.github.com>
2020-09-14 23:20:36 +0300

    Add files via upload

---
+++ Cv-Maxim-Durnea.py
@@ -0,0 +1,12 @@
+# Nume-Prenume
+print("Maxim Durnea")
+# Data,Luna,Anul nasterii
+print("29.03.2004")
+# Numar mobil
+print("+373060895344")
+# Adresa e-mail
+print("maxim.durnea@mail.ru")
+# Studii
+print("2011-2015 scoala primara Alexei Mateevici s.Rautel;2015-prezent IPLTME or.Balti")
+# Abilitati/Calitati
+print("Skateboarding la nivel mijlociu,am programat 2 jocuri in Unity3D folosind limbajul C# /Sunt o persoana rabdatoare,perseverenta si optimista,invat repede insa doar ceea ce imi place")
